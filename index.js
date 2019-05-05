e298df25c01fe6c3a1f9760e84c8455b00d28bdd
Sebastian Golian <sebastiangolian@gmail.com>
2019-05-05 21:25:32 +0200

    add access-control-allow

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser());
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/fail', (req, res) => res.status(403).json({msg: 'You are not allowed to access this'}));
